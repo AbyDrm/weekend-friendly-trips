7e759ab71c2f02a0ce7551de7f6fa75cf0cf20d7
Aby Drame <aby.drm@gmail.com>
2026-03-24 14:19:14 +0100

    Rename geocodage_xy to geo_point, extract latitude/longitude, and add distance_from_paris_km to cleaned festivals table

--- sql/cleaning/clean_festivals.sql
+++ sql/cleaning/clean_festivals.sql
@@ -20,6 +20,20 @@ SELECT
    annee_de_creation_d_festival AS creation_year,
    discipline_dominante AS category,
    periode_principale_de_deroulement_du_festival AS period,
-   geocodage_xy AS geo_coordinates
+   geocodage_xy AS geo_point,
+
+   -- Extract longitude and latitude from geocodage_xy
+   CAST(SPLIT(geocodage_xy, ",")[OFFSET(0)] AS FLOAT64) AS latitude,
+   CAST(SPLIT(geocodage_xy, ",")[OFFSET(1)] AS FLOAT64) AS longitude,
+
+   -- Distance from Paris (in kilometers)
+   ST_DISTANCE(
+    ST_GEOGPOINT(
+      CAST(SPLIT(geocodage_xy, ",")[OFFSET(1)] AS FLOAT64), -- longitude
+      CAST(SPLIT(geocodage_xy, ",")[OFFSET(0)] AS FLOAT64)  -- latitude
+    ), -- festival location
+    ST_GEOGPOINT(2.355, 48.856) -- Paris city center
+   ) / 1000 AS distance_from_paris_km
+
 
 FROM `weekend-friendly-trips-489113.raw.raw_festivals` 
